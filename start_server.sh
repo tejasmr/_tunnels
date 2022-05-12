a32f6aa993b7759507ec3759c16eb609d0684461
tezz-io <totejasmr@gmail.com>
2022-05-12 18:40:41 +0530

    Updated tunnels.json

--- start_server.sh
+++ start_server.sh
@@ -3,9 +3,11 @@ sleep 1
 ngrok tcp 22 > /dev/null &
 sleep 1
 curl  http://localhost:4040/api/tunnels > tunnels.json
-# git init
+git config user.name tezz-io
+git config user.email totejasmr@gmail.com
+git init
 git add .
 git commit -m "Updated tunnels.json"
 git branch -M main
-# git remote add origin git@github.com:tezz-io/_tunnels.git
+git remote add origin git@github.com:tezz-io/_tunnels.git
 git push -u origin main
