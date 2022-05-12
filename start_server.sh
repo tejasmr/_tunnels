9984c6ccca3443f598ec5757b29efe6b23e09151
tezz-io <totejasmr@gmail.com>
2022-05-12 18:50:16 +0530

    Updated tunnels.json

--- start_server.sh
+++ start_server.sh
@@ -5,9 +5,15 @@ sleep 1
 curl  http://localhost:4040/api/tunnels > tunnels.json
 git config user.name tezz-io
 git config user.email totejasmr@gmail.com
-git init
+DIR="/home/pi/ssh/.git"
+if [ ! -d "$DIR" ]; then
+  git init
+fi
 git add .
 git commit -m "Updated tunnels.json"
 git branch -M main
-git remote add origin git@github.com:tezz-io/_tunnels.git
+DIR="/home/pi/ssh/.git/refs/remotes/origin"
+if [ ! -d "$DIR" ]; then
+  git remote add origin git@github.com:tezz-io/_tunnels.git
+fi
 git push -u origin main
