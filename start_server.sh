3ac6f2fc39c168b48ff29f3f93f90f2d9c7ec57f
tezz-io <totejasmr@gmail.com>
2022-05-12 22:14:12 +0530

    Updated tunnels.json

--- start_server.sh
+++ start_server.sh
@@ -2,8 +2,9 @@ cd /home/pi/ssh
 killall ngrok
 sleep 1
 ngrok tcp 22 > /dev/null &
-sleep 1
+sleep 3
 curl  http://localhost:4040/api/tunnels > tunnels.json
+sleep 2
 echo $(cat tunnels.json | jq -r '.tunnels[0].public_url') > tunnels.json
 sed -i 's+tcp://0.tcp.in.ngrok.io:++g' tunnels.json
 git config user.name tezz-io
