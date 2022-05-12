ea7a58489f5f1052836dc2325d58740cd20a64e0
TejasS <tejasboss1904@gmail.com>
2022-05-12 18:29:06 +0530

    Updated tunnels.json

---
+++ start_server.sh
@@ -0,0 +1,7 @@
+curl  http://localhost:4040/api/tunnels > tunnels.json
+git init
+git add .
+git commit -m "Updated tunnels.json"
+git branch -M main
+git remote add origin git@github.com:tezz-io/_tunnels.git
+git push -u origin main
